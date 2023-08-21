9048f8a5cdfb81d6031c7bd2dd22b65330e7cfd2
Elievelton <elievelton@gmail.com>
2023-08-21 12:37:28 -0300

    correcoes

--- pages/work/index.js
+++ pages/work/index.js
@@ -19,15 +19,14 @@ const Work = () => {
         <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
           <motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className='h2 xl:mt-12'>My work <span className='text-accent'>.</span></motion.h2>
           <motion.p variants={fadeIn('up', 0.4)} initial="hidden" animate="show" exit="hidden" className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-            lorem fffffffffffffffffffffffffffffffffffffffffffffffffff.asas
-            sssssssssss aaaaaaaaa sssssssssssd sssssssssd sdsdsa.
-      
+            I develop modern websites using React, Next.js, and Node.js, ensuring high performance and interactivity. For content-centric platforms, I utilize WordPress to deliver user-friendly and robust solutions, blending the best of cutting-edge tech with proven systems.
+
           </motion.p>
         </div>
         {/*slider*/}
         <motion.div variants={fadeIn('down', 0.6)} initial="hidden" animate="show" exit="hidden" className='h-full xl:max-w-[65%] '>
 
-        <WorkSlider/>
+          <WorkSlider />
         </motion.div>
 
       </div>
